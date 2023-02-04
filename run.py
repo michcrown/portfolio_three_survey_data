05d1f8fa76ac79180b093c62093ef64bb2da4057
michcrown <michcrownover@gmail.com>
2023-02-04 13:21:23 +0000

    adding porfolio requirements as reminder

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ def import_survey_results(file_path):
     return data
 
 # function to analyse survey data in a basic way
+# will need more detail - maybe on what information this parses? 
 def analyze_survey_data(data):
    
     mean = np.mean(data)
@@ -32,7 +33,9 @@ def analyze_survey_data(data):
     print(key_findings)
     return key_findings
 
-# write the data to a CSV file using pandas
+# write the data to a CSV file using pandas 
+# maybe will need to be able "appropriate file" - maybe need PDF's too?
 def export_results(data, file_path):
     data.to_csv(file_path, index=False)
     print("Results exported successfully to", file_path)
+
